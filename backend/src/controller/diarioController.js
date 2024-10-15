5293549c122f095ce6c9015f605b3b1b135552f6
CesarRibeiro21 <cesartertulianort@gmail.com>
2024-10-15 16:51:08 -0300

    DiarioController feito

--- backend/src/controller/diarioController.js
+++ backend/src/controller/diarioController.js
@@ -6,7 +6,7 @@ const endpoints = Router();
 endpoints.get('/diario/', async (req, resp) => {
     try{
         let registro= await db.consultarDiario();
-        resp.send(registros);
+        resp.send(registro);
     }
     catch (err){
         resp.status(400).send({
@@ -15,3 +15,61 @@ endpoints.get('/diario/', async (req, resp) => {
     }
 })
 
+endpoints.post('/diario/add/', async (req, resp) => {
+    try{
+        let id = req.params.id;
+        let pessoa = req.body;
+        
+        let linhasAfetadas= await db.inserirDiario(id, pessoa)
+        if (linhasAfetadas >= 1){
+            resp.send();
+        }
+        else{
+            resp.status(404).send({ erro : 'Nenhum registro encontrado'})
+        }
+    }
+    catch (err){
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
+
+endpoints.put('/alterar/diario/:id', async (req, resp) =>{
+    try{
+        let id = req.params.id;
+        let pessoa = req.body;
+
+        let linhasAfetadas = await db.alterarDiario(id, pessoa);
+        if(linhasAfetadas >= 1){
+            resp.send();
+        }
+        else{
+           resp.status(404).send({ erro : 'Nenhum registro encontrado'})
+        }
+    }
+    catch (err){
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
+
+endpoints.delete('/deletar/listaNegra/:id', async (req, resp) => {
+    try {
+        let id = req.params.id;
+
+        let linhasAfetadas = await db.deletarDiario(id);
+        if (linhasAfetadas >= 1) {
+            resp.send();
+        }
+        else {
+            resp.status(404).send({ erro: 'Nenhum registro encontrado' })
+        }
+    }
+    catch (err) {
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
